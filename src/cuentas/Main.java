1318b8d60093582fa1c86535abc114e8f4cc964d
Fani <eanidos@gmail.com>
2026-03-07 22:55:28 +0100

    Refactorización: Añadir parametro al metodo operativa_cuenta

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 695f);
     }
 
-	private static void operativa_cuenta(CCuenta cuenta1) {
+	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		double saldoActual;
 		saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
@@ -20,7 +20,7 @@ public class Main {
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
